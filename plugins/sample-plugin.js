a549132018b445b6ff89fb14d609e522b482998d
DarkNebula2720 <brandonleistner2012@outlook.com>
2025-06-11 15:44:38 -0500

    Create sample-plugin.js

---
+++ plugins/sample-plugin.js
@@ -0,0 +1,8 @@
+/** Sample plugin file structure */
+export const name = "Sample Plugin";
+export const version = "1.0.0";
+
+export async function main(ns) {
+    ns.tprint("🔧 Sample Plugin Loaded!");
+}
+
